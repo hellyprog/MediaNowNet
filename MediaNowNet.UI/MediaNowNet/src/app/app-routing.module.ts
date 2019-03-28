c5232727fc6d1c94e76e4ad6231f75295a1d3ca0
Romaniv Volodymyr <romaniv.volodumur@gmail.com>
2019-03-28 18:11:52 +0200

    Added lazy welcome module to route

--- MediaNowNet.UI/MediaNowNet/src/app/app-routing.module.ts
+++ MediaNowNet.UI/MediaNowNet/src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { Routes, RouterModule, PreloadAllModules } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'welcome',
+    loadChildren: './welcome/welcome.module#WelcomeModule'
+  }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {
+    preloadingStrategy: PreloadAllModules,
+    enableTracing: true
+  })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
---
+++ MediaNowNet.UI/MediaNowNet/src/app/welcome/index.ts
@@ -0,0 +1,2 @@
+export * from './welcome.module';
+export * from './pages/welcome/welcome.component';
