0196a8707b13e1300ec3f2af6a6bf5ea73433527
Romaniv Volodymyr <romaniv.volodumur@gmail.com>
2019-04-06 17:56:16 +0300

    Added header color change on scroll

--- MediaNowNet.UI/MediaNowNet/src/app/core/components/header/header.component.ts
+++ MediaNowNet.UI/MediaNowNet/src/app/core/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -7,8 +7,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
   isActiveMenu = false;
+  isPageScrolled = false;
 
   constructor() { }
+
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    const offset = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
+    if (offset > 100) {
+      this.isPageScrolled = true;
+    } else {
+      this.isPageScrolled = false;
+    }
+  }
 
   ngOnInit() {
   }
