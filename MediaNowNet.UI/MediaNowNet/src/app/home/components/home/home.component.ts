90b994bfdfa5cfe8e90d6716399ba9ff2080426d
Romaniv Volodymyr <romaniv.volodumur@gmail.com>
2019-05-16 17:57:04 +0300

    Draft structure of home page

--- MediaNowNet.UI/MediaNowNet/src/app/home/components/home/home.component.ts
+++ MediaNowNet.UI/MediaNowNet/src/app/home/components/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, HostBinding } from '@angular/core';
 import { Title } from '@angular/platform-browser';
+import { UserService } from '../../../core/services/user/user.service';
 
 @Component({
   selector: 'app-home',
@@ -10,10 +11,13 @@ export class HomeComponent implements OnInit {
   classes: string = null;
   showSubscriptionMessage: boolean;
 
-  constructor(private titleService: Title) {
+  constructor(
+    private titleService: Title,
+    private userService: UserService
+  ) {
     this.classes = 'home-component';
     this.titleService.setTitle('Home - MediaNowNet');
-    this.showSubscriptionMessage = !localStorage.isLoggedIn;
+    this.showSubscriptionMessage = this.userService.login();
   }
 
   ngOnInit() {
