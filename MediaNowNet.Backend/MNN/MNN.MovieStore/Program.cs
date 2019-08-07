e2d292d89e423ebccf38ac8ef2049295b17f7343
Romaniv Volodymyr <romaniv.volodumur@gmail.com>
2019-08-07 16:50:21 +0300

    Removed old backend solution added new draft one

--- MediaNowNet.Backend/MNN/MNN.MovieStore/Program.cs
+++ MediaNowNet.Backend/MNN/MNN.MovieStore/Program.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
-namespace MediaNowNet.Channel
+namespace MNN.MovieStore
 {
     public class Program
     {
--- MediaNowNet.Backend/MNN/MNN.MovieStore/Startup.cs
+++ MediaNowNet.Backend/MNN/MNN.MovieStore/Startup.cs
@@ -4,13 +4,14 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 
-namespace MediaNowNet.Channel
+namespace MNN.MovieStore
 {
     public class Startup
     {
@@ -21,26 +22,25 @@ namespace MediaNowNet.Channel
 
         public IConfiguration Configuration { get; }
 
+        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
-            services.AddAuthentication("Bearer")
-                .AddJwtBearer("Bearer", options =>
-                {
-                    options.Authority = "http://localhost:5000";
-                    options.RequireHttpsMetadata = false;
-                    options.Audience = "api1";
-                });
         }
 
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
+            else
+            {
+                app.UseHsts();
+            }
 
-            app.UseAuthentication();
+            app.UseHttpsRedirection();
             app.UseMvc();
         }
     }
